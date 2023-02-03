4bdb7c823ac17ea6de4c8dd429929b2c1838918e
Dmitry Sky <dmitry.sky@icloud.com>
2023-02-03 11:23:27 -0700

    Helper hardhat config to store eth/use price feed address and other deploy network details like chain id's

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,5 +1,17 @@
-function deployFunc() {
-  console.log("Deploying FundMe...");
-}
+const { networkConfig } = require("../helper-hardhat-config");
 
-module.exports.default = deployFunc;
+// hre = hardhat runtime environment
+module.exports = async (hre) => {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = network.config.chainId;
+
+  const ethUsdPriceFeedAddress = networkConfig[chainId][ethUsdPriceFeed];
+
+  await deploy("FundMe", {
+    from: deployer,
+    args: [],
+    log: true,
+  });
+};
