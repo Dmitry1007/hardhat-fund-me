441ee92c5e41048f77a4f5af597e88bff5c8bca6
Dmitry Sky <dmitry.sky@icloud.com>
2023-02-06 11:18:01 -0700

    Withdraw function should work with multiple funders

--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -52,7 +52,7 @@ describe("FundMe", async function () {
       await fundMe.fund({ value: fundAmount });
     });
 
-    it("Withdraw ETH from a single funder", async function () {
+    it("Withdraw ETH with a single funder", async function () {
       // Arrange
       const startingFundMeBalance = await fundMe.provider.getBalance(
         fundMe.address
@@ -79,6 +79,51 @@ describe("FundMe", async function () {
         startingFundMeBalance.add(startingDeployerBalance).toString(),
         endingDeployerBalance.add(gasCost).toString()
       );
+    });
+
+    it("Allows us to withdraw with multiple funders", async function () {
+      // Arrange
+      const fundAmount = ethers.utils.parseEther("1");
+
+      const accounts = await ethers.getSigners();
+      for (let i = 1; i < 6; i++) {
+        const fundMeConnectedContract = await fundMe.connect(accounts[i]);
+        await fundMeConnectedContract.fund({ value: fundAmount });
+      }
+
+      const startingFundMeBalance = await fundMe.provider.getBalance(
+        fundMe.address
+      );
+      const startingDeployerBalance = await fundMe.provider.getBalance(
+        deployer
+      );
+
+      // Act
+      const transactionResponse = await fundMe.withdraw();
+      const transactionReceipt = await transactionResponse.wait(1);
+      const { gasUsed, effectiveGasPrice } = transactionReceipt;
+      const gasCost = gasUsed.mul(effectiveGasPrice);
+
+      const endingFundMeBalance = await fundMe.provider.getBalance(
+        fundMe.address
+      );
+      const endingDeployerBalance = await fundMe.provider.getBalance(deployer);
+
+      // Assert
+      assert.equal(endingFundMeBalance, 0);
+      assert.equal(
+        startingFundMeBalance.add(startingDeployerBalance).toString(),
+        endingDeployerBalance.add(gasCost).toString()
+      );
+      // Make sure the funders are reset properly
+      await expect(fundMe.getFunder(0)).to.be.reverted;
+
+      for (let i = 1; i < 6; i++) {
+        assert.equal(
+          await fundMe.getAddressToAmountFunded(accounts[i].address),
+          0
+        );
+      }
     });
   });
 });
