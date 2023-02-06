7888dbff8996e1600f5181b8c037957428c01a95
Dmitry Sky <dmitry.sky@icloud.com>
2023-02-06 11:36:51 -0700

    More explicit error

--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -56,7 +56,9 @@ describe("FundMe", async function () {
       const accounts = await ethers.getSigners();
       const attacker = accounts[1];
       const attackerConnectedContract = await fundMe.connect(attacker);
-      await expect(attackerConnectedContract.withdraw()).to.be.reverted;
+      await expect(attackerConnectedContract.withdraw()).to.be.revertedWith(
+        "FundMe__NotOwner"
+      );
     });
 
     it("Withdraw ETH with a single funder", async function () {
